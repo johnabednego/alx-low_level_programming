67db6eb4d29da98f47183e8949611085d884a6f9
johnabednego <jajilima@st.ug.edu.gh>
2023-06-16 04:53:22 +0000

    done

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -2,8 +2,8 @@
 
 int main(void)
 {
-const char *quote = "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n";
-    
+const char *quote = 
+"and that piece of art is useful\" - Dora Korpar, 2015-10-19\n";  
 write(2, quote, 59);
 return 1;
 }
