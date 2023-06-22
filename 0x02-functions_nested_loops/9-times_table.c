861d688cf1699ef04fffaf8295a9c650a3f8f047
johnabednego <jajilima@st.ug.edu.gh>
2023-06-22 01:22:31 +0000

    done

--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -5,28 +5,28 @@
  */
 void times_table(void)
 {
-	int row, column, product;
+int row, column, product;
 
-	for (row = 0; row <= 9; row++)
-	{
-		_putchar('0'); /* Print the first column (0) */
+for (row = 0; row <= 9; row++)
+{
+_putchar('0'); /* Print the first column (0) */
 
-		for (column = 1; column <= 9; column++)
-		{
-			_putchar(','); /* Print the separator */
-			_putchar(' ');
+for (column = 1; column <= 9; column++)
+{
+_putchar(','); /* Print the separator */
+_putchar(' ');
 
-			product = row * column;
+product = row * column;
 
-			if (product <= 9)
-				_putchar(' '); /* Align single-digit numbers */
+if (product <= 9)
+_putchar(' '); /* Align single-digit numbers */
 
-			if (product > 9)
-				_putchar((product / 10) + '0'); /* Print tens digit */
+if (product > 9)
+_putchar((product / 10) + '0'); /* Print tens digit */
 
-			_putchar((product % 10) + '0'); /* Print ones digit */
-		}
+_putchar((product % 10) + '0'); /* Print ones digit */
+}
 
-		_putchar('\n'); /* Move to the next row */
-	}
+_putchar('\n'); /* Move to the next row */
+}
 }
