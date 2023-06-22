e0d67450e1965846650cbcab94ee677017944934
johnabednego <jajilima@st.ug.edu.gh>
2023-06-22 01:18:41 +0000

    done

---
+++ 0x02-functions_nested_loops/main.h
@@ -0,0 +1,18 @@
+#ifndef MAIN_H
+#define MAIN_H
+
+int _islower(int c);
+int _isalpha(int c);
+int print_sign(int n);
+int _abs(int n);
+int _putchar(char c);
+int print_last_digit(int n);
+
+void times_table(void);
+int _putchar(char c);
+
+int add(int a, int b);
+
+void print_to_98(int n);
+
+#endif /* MAIN_H */
