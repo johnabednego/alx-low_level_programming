c8cf386cd96bb14d3bc6b3609635acdb96e05438
johnabednego <jajilima@st.ug.edu.gh>
2023-06-27 14:46:07 +0000

    done

---
+++ 0x05-pointers_arrays_strings/101-keygen.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+
+#define PASSWORD_LENGTH 10
+
+/**
+ * main - Entry point of the program.
+ *
+ * Return: Always 0.
+ */
+int main(void)
+{
+char password[PASSWORD_LENGTH + 1];
+int i;
+
+srand(time(0));
+
+for (i = 0; i < PASSWORD_LENGTH; i++)
+{
+password[i] = rand() % 94 + 33; // Generate a random printable ASCII character
+}
+password[i] = '\0'; // Add null terminator at the end of the password
+
+printf("%s\n", password);
+
+return 0;
+}
--- 0x05-pointers_arrays_strings/main.h
+++ 0x05-pointers_arrays_strings/main.h
@@ -10,5 +10,7 @@ void rev_string(char *s);
 void puts_half(char *str);
 void print_array(int *a, int n);
 char *_strcpy(char *dest, char *src);
+int _atoi(char *s);
+int main(void);
 
 #endif /* MAIN_H */
