02b64b5408084f1c48e062c234b6e20e500ab2d3
johnabednego <jajilima@st.ug.edu.gh>
2023-07-06 00:15:58 +0000

    done

--- 0x08-recursion/101-wildcmp.c
+++ 0x08-recursion/101-wildcmp.c
@@ -1,6 +1,6 @@
 #include "main.h"
 
-int match(char *s1, char *s2);
+int wildcmp_helper(char *s1, char *s2);
 
 /**
  * wildcmp - Compares two strings and returns 1 if they can be considered
@@ -12,32 +12,29 @@ int match(char *s1, char *s2);
  */
 int wildcmp(char *s1, char *s2)
 {
-if (*s2 == '*')
-{
-if (*(s2 + 1) == '\0')
-return (1);
-return (match(s1, s2 + 1));
-}
-return (match(s1, s2));
+return (wildcmp_helper(s1, s2));
 }
 
 /**
-* match - Recursively compares two strings and returns 1 if they can be
-*         considered identical, otherwise returns 0.
-* @s1: The first string to compare.
-* @s2: The second string to compare.
-*
-* Return: 1 if the strings can be considered identical, otherwise 0.
-*/
-int match(char *s1, char *s2)
+ * wildcmp_helper - Recursive helper function to compare two strings.
+ * @s1: The first string to compare.
+ * @s2: The second string to compare.
+ *
+ * Return: 1 if the strings can be considered identical, otherwise 0.
+ */
+int wildcmp_helper(char *s1, char *s2)
 {
 if (*s2 == '*')
-return ((match(s1, s2 + 1)) || (match(s1 + 1, s2)));
-else if (*s2 == '\0' && *s1 == '\0')
+{
+if (*(s2 + 1) == '\0') /* Trailing '*' matches anything */
 return (1);
-else if (*s2 == '\0' || *s1 == '\0')
-return (0);
-else if (*s1 == *s2)
-return (match(s1 + 1, s2 + 1));
-return (0);
+if (*s1 != '\0' && wildcmp_helper(s1 + 1, s2)) /* Match '*' with a character */
+return (1);
+return (wildcmp_helper(s1, s2 + 1)); /* Skip '*' and continue */
+}
+if (*s1 == '\0' && *s2 == '\0') /* Both strings reached the end */
+return (1);
+if (*s1 == *s2 && *s1 != '\0' && *s2 != '\0') /* Characters match */
+return (wildcmp_helper(s1 + 1, s2 + 1)); /* Continue matching */
+return (0); /* Characters don't match */
 }
