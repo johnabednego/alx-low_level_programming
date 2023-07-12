198fcae089caf6cfc7307ab141e810196fad1802
johnabednego <jajilima@st.ug.edu.gh>
2023-07-12 01:15:17 +0000

    done

--- 0x0B-malloc_free/101-strtow.c
+++ 0x0B-malloc_free/101-strtow.c
@@ -10,19 +10,24 @@ int count_words(char *str)
 {
 int count = 0;
 int i = 0;
+
 while (str[i] != '\0')
 {
 while (str[i] == ' ')
 i++;
+
 if (str[i] != '\0')
 {
 count++;
+
 while (str[i] != ' ' && str[i] != '\0')
 i++;
 }
 }
+
 return (count);
 }
+
 /**
 * copy_word - Copies a word from the source string to the destination string.
 * @src: The source string containing the word
@@ -33,77 +38,72 @@ return (count);
 void copy_word(char *src, int start, int end, char *dest)
 {
 int i;
+
 for (i = start; i < end; i++)
 dest[i - start] = src[i];
+
 dest[i - start] = '\0';
-}
-/**
-* allocate_words - Allocates memory for the words in the string.
-* @str: The string to split
-* @word_count: The number of words in the string
-* Return: Pointer to an array of strings (words), or NULL if it fails
-*/
-char **allocate_words(char *str, int word_count)
-{
-char **words;
-int i;
-words = malloc(sizeof(char *) * (word_count + 1));
-if (words == NULL)
-return (NULL);
-for (i = 0; i < word_count; i++)
-{
-words[i] = malloc(sizeof(char) * (strlen(str) + 1));
-if (words[i] == NULL)
-{
-for (i = 0; i < word_count; i++)
-free(words[i]);
-free(words);
-return (NULL);
-}
-}
-return (words);
 }
 
 /**
-* extract_words - Extracts the words from the string and stores them in the array.
-* @str: The string to split
-* @words: The array to store the words
-* Return: void
-*/
-void extract_words(char *str, char **words)
-{
-int i = 0;
-int j = 0;
-int k = 0;
-while (str[i] != '\0')
-{
-while (str[i] == ' ')
-i++;
-j = i;
-while (str[i] != ' ' && str[i] != '\0')
-i++;
-copy_word(str, j, i, words[k]);
-k++;
-}
-words[k] = NULL;
-}
-/**
 * strtow - Splits a string into words.
 * @str: The string to split
+*
 * Return: Pointer to an array of strings (words), or NULL if it fails
 */
 char **strtow(char *str)
 {
 char **words;
-int word_count;
+int i, j, k;
+int word_count = 0;
+int word_length = 0;
+
 if (str == NULL || str[0] == '\0')
 return (NULL);
+
 word_count = count_words(str);
+
 if (word_count == 0)
 return (NULL);
-words = allocate_words(str, word_count);
+
+words = malloc(sizeof(char *) * (word_count + 1));
+
 if (words == NULL)
 return (NULL);
-extract_words(str, words);
+
+i = 0;
+k = 0;
+
+while (str[i] != '\0' && k < word_count)
+{
+while (str[i] == ' ')
+i++;
+
+j = i;
+
+while (str[i] != ' ' && str[i] != '\0')
+{
+word_length++;
+i++;
+}
+
+words[k] = malloc(sizeof(char) * (word_length + 1));
+
+if (words[k] == NULL)
+{
+for (i = 0; i < k; i++)
+free(words[i]);
+free(words);
+return (NULL);
+}
+
+copy_word(str, j, i, words[k]);
+
+word_length = 0;
+k++;
+}
+
+words[k] = NULL;
+
 return (words);
 }
