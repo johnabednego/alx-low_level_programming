66b791ee09622f15957b35b8465c242c95503f63
johnabednego <jajilima@st.ug.edu.gh>
2023-08-07 18:47:54 +0000

    done

--- 0x15-file_io/main.h
+++ 0x15-file_io/main.h
@@ -10,14 +10,4 @@ ssize_t read_textfile(const char *filename, size_t letters);
 int create_file(const char *filename, char *text_content);
 int append_text_to_file(const char *filename, char *text_content);
 
-/* Function prototypes */
-void print_error(void);
-void display_elf_header(char *filename);
-
-/* Strings corresponding to OS/ABI values */
-extern const char *os_abi_strings[];
-
-/* Strings corresponding to ELF type values */
-extern const char *elf_type_strings[];
-
 #endif
